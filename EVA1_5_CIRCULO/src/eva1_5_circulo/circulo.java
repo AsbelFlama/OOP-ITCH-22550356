a9f89924b73b7075cc748a89e6d8cbc9c4a765f1
AsbelFlama <124391864+AsbelFlama@users.noreply.github.com>
2023-02-17 12:50:18 -0600

    Add files via upload

--- EVA1_5_CIRCULO/src/eva1_5_circulo/circulo.java
+++ EVA1_5_CIRCULO/src/eva1_5_circulo/circulo.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eva1_5_circulo;
 
 import java.util.Scanner;
@@ -13,30 +12,34 @@ import java.util.Scanner;
  * @author invitado
  */
 class circulo {
+
     private float radio;
     private float area;
     private float perimetro;
     private float diametro;
-    public float getRadio(){
+
+    public float getRadio() {
         return radio;
     }
-    public void setRadio(float rad){
-        radio=rad;
+
+    public void setRadio(float rad) {
+        radio = rad;
     }
-   public void calculos(){
-    Scanner r =new Scanner(System.in);
-       System.out.println("Inserte radio: ");       
-       radio=r.nextFloat();
-       if(radio<0.1){
-       System.out.println("Inserte un Número valido");
-       }else{
-       diametro=2*(radio);
-       perimetro=(float) ((diametro)*Math.PI);
-       area = (float) (Math.pow(2, radio)*Math.PI);
-       System.out.println("El area es " +area);
-       System.out.println("El perimetro es " +perimetro);       
-       System.out.println("El diametro es" + diametro);       
-       }
-       
-   }
+
+    public void calculos() {
+        Scanner r = new Scanner(System.in);
+        System.out.println("Inserte radio: ");
+        radio = r.nextFloat();
+        if (radio < 0.1) {
+            System.out.println("Inserte un Número valido");
+        } else {
+            diametro = 2 * (radio);
+            perimetro = (float) ((diametro) * Math.PI);
+            area = (float) (Math.pow(2, radio) * Math.PI);
+            System.out.println("El area es " + area);
+            System.out.println("El perimetro es " + perimetro);
+            System.out.println("El diametro es" + diametro);
+        }
+
+    }
 }
