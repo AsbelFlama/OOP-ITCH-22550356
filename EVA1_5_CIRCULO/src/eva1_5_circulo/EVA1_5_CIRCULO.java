0d8787ca410039201f2b70fc0b80b816ab2283a4
AsbelFlama <124391864+AsbelFlama@users.noreply.github.com>
2023-02-17 12:47:15 -0600

    Add files via upload

---
+++ EVA1_5_CIRCULO/src/eva1_5_circulo/EVA1_5_CIRCULO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_5_circulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_5_CIRCULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        circulo circ= new circulo();
+        circ.calculos();
+        
+        
+        
+    }
+    
+}
