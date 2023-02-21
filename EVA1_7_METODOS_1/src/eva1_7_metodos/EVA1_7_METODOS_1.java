90b0f1f5dc08df9357b95d4c022a1a8e487d2d1f
AsbelFlama <124391864+AsbelFlama@users.noreply.github.com>
2023-02-21 12:45:20 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos/EVA1_7_METODOS_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_7_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_7_METODOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        myMethod();
+    }
+    
+    public static void myMethod(){
+        System.out.println("You have called me master!!");
+    }
+    
+}
